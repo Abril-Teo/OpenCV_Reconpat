028ec6324feaabf7418ab201ba59fa40824a30bc
Teo Abril <teo.abril10@gmail.com>
2023-06-01 12:16:07 -0300

    contornos

--- main.py
+++ main.py
@@ -3,27 +3,26 @@ import numpy as np
 import cv2
 import pytesseract
 import skimage
+import matplotlib.pyplot as plt
 
 
 def mostrarImagen(path, img):
     cv2.imshow(path, img)
-    cv2.waitKey(2000)
+    cv2.waitKey(0)
 
 
 
-path = 'imgs/000.png'
+path = 'imgs/001.png'
 
-normal = cv2.imread(path, 1)
-mostrarImagen(path, normal)
+imagen = cv2.imread(path)
+mostrarImagen(path, imagen)
 
-gris = cv2.imread(path, 0)
+gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 mostrarImagen(path, gris)
 
 threshold = cv2.threshold(gris, 170, 255, cv2.THRESH_BINARY_INV)[1]
 mostrarImagen(path, threshold)
 
-contornos = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
-canvas = np.zeros_like(path)
-contornos2 = cv2.drawContours(canvas, contornos, -1, (0,255,0), 3)
-mostrarImagen(path,contornos2)
-"""mostrarImagen(path, contornos)"""
+contornos, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(threshold, contornos, -1, (0, 255, 0), 2)
+mostrarImagen(path, threshold)
