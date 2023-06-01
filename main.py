ab165017484935413cfb77de8c8bc256d9144c0b
Tomas <tomyasis2005@gmail.com>
2023-05-31 23:19:10 -0300

    Optimizacion codigo

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
+from cv2 import contourArea
 import numpy as np
 import cv2
 import pytesseract
 import skimage
 
 
-def grayscale(img):
-    gray = cv2.imread(img, 0)
-    cv2.imshow(img, gray)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    return gray
+def mostrarImagen(path, img):
+    cv2.imshow(path, img)
+    cv2.waitKey(2000)
 
-def threshold(img_gray, img):
-    threshold = cv2.threshold(img_gray, 170, 255, cv2.THRESH_BINARY_INV)[1]
-    cv2.imshow(img, threshold)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    return threshold
 
- 
-img = 'imgs/000.png'
 
-img_gray = grayscale(img)
-threshold(img_gray, img)
+path = 'imgs/000.png'
 
+normal = cv2.imread(path, 1)
+mostrarImagen(path, normal)
+
+gris = cv2.imread(path, 0)
+mostrarImagen(path, gris)
+
+threshold = cv2.threshold(gris, 170, 255, cv2.THRESH_BINARY_INV)[1]
+mostrarImagen(path, threshold)
+
+contornos = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
+canvas = np.zeros_like(path)
+contornos2 = cv2.drawContours(canvas, contornos, -1, (0,255,0), 3)
+mostrarImagen(path,contornos2)
+"""mostrarImagen(path, contornos)"""
